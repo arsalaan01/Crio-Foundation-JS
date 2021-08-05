f77efea413bf770d991d4e5e274a4f76bdeeeb3e
arsalaan <arsalaan0101@gmail.com>
2021-08-05 16:35:23 +0000

    activity4

--- Session2/Activity3_SwitchCase.js
+++ Session2/Activity3_SwitchCase.js
@@ -9,19 +9,27 @@
 
 // switch case
 
-let val = '1';
+let val = 'c';
 let answer = "";
 switch(val){
   case '1':
+  case 'A':
+  case 'a':
   answer='alpha';
   break;
   case '2':
+  case 'B':
+  case 'b':
   answer='beta';
   break;
   case '3':
+  case 'C':
+  case 'c':
   answer='gamma';
   break;
   case '4':
+  case 'D':
+  case 'd':
   answer='delta';
   break;
   default:
